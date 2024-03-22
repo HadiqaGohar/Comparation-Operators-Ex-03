32f0272edd7a68a0cacd6d96686b69f772353ba8
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2024-03-22 08:19:59 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+//COMPARATION OPERATORS
+//FOOD
+var food = "Biryani";
+if (food == "Tikka") {
+    console.log("You don't like Tikka");
+}
+if (food == "Biryani") {
+    console.log("You like Biryani");
+}
+else {
+    console.log("You really like Biryani");
+}
---
+++ main.ts
@@ -0,0 +1,9 @@
+//COMPARATION OPERATORS
+//FOOD
+let food = "Biryani"
+if (food == "Tikka") {
+    console.log ("You don't like Tikka");  
+} if (food == "Biryani") {
+    console.log ("You like Biryani")
+} else {
+    console.log ("You really like Biryani")}
